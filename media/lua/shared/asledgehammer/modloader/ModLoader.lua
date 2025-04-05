a0d7533b7bb45589476628ce64e3f1a1be96ca8b
JabDoesThings <JabDoesThings@github.com>
2025-04-05 13:44:54 -0500

    API rewrite & Addition of file whitelists.

--- media/lua/shared/asledgehammer/modloader/ModLoader.lua
+++ media/lua/shared/asledgehammer/modloader/ModLoader.lua
@@ -4,7 +4,12 @@
 --- @author asledgehammer, JabDoesThings, 2024
 --]]
 
+--- @alias FileRequestCallback fun(result: 0 | 1, data: string | nil): string | void
+
+--- @alias ModLoaderWhitelist table<string, string>
+
 local Packet = require 'asledgehammer/network/Packet';
+local ZedUtils = require 'asledgehammer/util/ZedUtils';
 
 local IS_CLIENT = isClient();
 local IS_SERVER = isServer();
@@ -12,13 +17,18 @@ local IS_SERVER = isServer();
 -- This library is only used for multiplayer sessions.
 if not IS_CLIENT and not IS_SERVER then return end
 
---- @alias ServerFileRequestCallback fun(module: string, path: string, result: 0 | 1, data: string | nil): string | void
-
 --- @type table<string, string>
-local CACHED_FILES = {};
+local CLIENT_CACHED_FILES = {};
 
---- @type table<string, table<string, ServerFileRequestCallback>>
+--- @type table<string, ModLoaderWhitelist>
+local CACHED_WHITELISTS = {};
+
+--- @type table<string, table<string, FileRequestCallback>>
 local CALLBACKS = {};
+
+local info = function(message)
+    print('[ModLoader] :: ' .. tostring(message));
+end
 
 local ModLoader = {
     --- The ID used to communicate with the client and server.
@@ -31,32 +41,42 @@ local ModLoader = {
 
     --- The result passed when the file is not found.
     --- @type 1
-    RESULT_FILE_NOT_FOUND = 1
+    RESULT_FILE_NOT_FOUND = 1,
+
+    --- A simple key to encrypt packets. A server-specific mod can change this to add *some* security against templated
+    --- interception hack for the client.
+    ---
+    --- NOTE: This is not a one-size-fits-all for security. Implementations of ModLoader API can handle security when
+    --- processing files.
+    SIMPLE_KEY = 'ModLoader'
 };
 
 --- @param module string
---- @param path string
+--- @param id string
 ---
 --- @return boolean
-local function cacheExists(module, path)
-    return CACHED_FILES[string.lower(module .. '/' .. path)] ~= nil;
+local function cacheExists(module, id)
+    local name = string.lower(module .. '/' .. id);
+    return CLIENT_CACHED_FILES[name] ~= nil;
 end
 
 --- @param module string
---- @param path string
+--- @param id string
 ---
 --- @return string | nil
-local function getCachedFile(module, path)
-    return CACHED_FILES[string.lower(module .. '/' .. path)];
+local function getCachedFile(module, id)
+    local name = string.lower(module .. '/' .. id);
+    return CLIENT_CACHED_FILES[name];
 end
 
 --- @param module string
---- @param path string
+--- @param id string
 --- @param data string
 ---
 --- @return void
-local function cacheFile(module, path, data)
-    CACHED_FILES[string.lower(module .. '/' .. path)] = data;
+local function cacheFile(module, id, data)
+    local name = string.lower(module .. '/' .. id);
+    CLIENT_CACHED_FILES[name] = data;
 end
 
 --- @param uri string
@@ -86,96 +106,110 @@ end
 
 --- @param player IsoPlayer
 --- @param module string
---- @param path string
---- @param cache boolean
+--- @param id string
 ---
 --- @return void
-local function onRequestServerFile(player, module, path, cache)
-    -- If the file is requested cached and is in the cache, grab it & send it.
-    if cache and cacheExists(module, path) then
-        local cachedFile = getCachedFile(module, path);
+local function onRequestServerFile(player, module, id)
 
+    local idRequested = id;
+
+    local uri = 'ModLoader/mods/' .. module .. '/';
+
+    --- @type ModLoaderWhitelist | nil
+    local whitelist;
+    if CACHED_WHITELISTS[uri] then
+        whitelist = CACHED_WHITELISTS[uri];
+    else
+        local data = readFile(uri .. 'whitelist.lua');
+        if data then
+            whitelist = loadstring(data)();
+            CACHED_WHITELISTS[uri] = whitelist;
+        end
+    end
+
+    if whitelist then
+
+        id = whitelist[id];
+        if not id then
+            local packet = Packet(ModLoader.MODULE_ID, 'request_server_file', {
+                module = module,
+                id = idRequested,
+                data = nil,
+                result = ModLoader.RESULT_FILE_NOT_FOUND
+            });
+
+            packet:encrypt(ModLoader.SIMPLE_KEY, function() packet:sendToPlayer(player) end);
+
+            info("Requested item not found in whitelist: \"" .. id .. "\"");
+
+            return;
+        end
+    end
+
+    -- If the file is requested cached and is in the cache, grab it & send it.
+    if cacheExists(module, id) then
+        local cachedFile = getCachedFile(module, id);
         local packet = Packet(ModLoader.MODULE_ID, 'request_server_file', {
             module = module,
-            path = path,
+            id = idRequested,
             data = cachedFile,
             result = ModLoader.RESULT_SUCCESS
         });
-
-        packet:encrypt('ModLoader', function()
-            packet:sendToPlayer(player);
-        end);
-
+        packet:encrypt(ModLoader.SIMPLE_KEY, function() packet:sendToPlayer(player) end);
         return;
     end
 
-    local uri = 'ModLoader/mods/' .. module .. '/' .. path;
-    local data = readFile(uri);
+    local data = readFile(uri .. id);
 
     -- A nil reader indicates a bad path or a missing file.
     if not data then
         local packet = Packet(ModLoader.MODULE_ID, 'request_server_file', {
             module = module,
-            path = path,
+            id = idRequested,
             data = nil,
             result = ModLoader.RESULT_FILE_NOT_FOUND
         });
-
-        packet:encrypt('ModLoader', function()
-            packet:sendToPlayer(player);
-        end);
-
-        print('[ModLoader] :: File not found: ' .. uri);
-
+        packet:encrypt(ModLoader.SIMPLE_KEY, function() packet:sendToPlayer(player) end);
+        info('File not found: ' .. uri);
         return;
     end
 
     local packet = Packet(ModLoader.MODULE_ID, 'request_server_file', {
         module = module,
-        path = path,
+        id = idRequested,
         data = data,
         result = ModLoader.RESULT_SUCCESS
     });
 
-    packet:encrypt('ModLoader', function()
-        packet:sendToPlayer(player);
-    end);
-
-    ---------------------------------------
-    -- Cache the file to be recalled later.
-    if cache then
-        cacheFile(module, path, data);
-    end
-    ---------------------------------------
+    packet:encrypt(ModLoader.SIMPLE_KEY, function() packet:sendToPlayer(player) end);
 end
 
 --- @param module string
---- @param path string
+--- @param id string
 --- @param result 0 | 1
 --- @param data string
 ---
 --- @return void
-local function onReceiveServerFile(module, path, result, data)
-    if CALLBACKS[module][path] then
+local function onReceiveServerFile(module, id, result, data)
+    if CALLBACKS[module][id] then
         -- Pop callback function.
-        local callback = CALLBACKS[module][path];
-        CALLBACKS[module][path] = nil;
+        local callback = CALLBACKS[module][id];
+        CALLBACKS[module][id] = nil;
 
         if type(callback) == 'function' then
             pcall(function()
-                callback(module, path, result, data);
+                callback(result, data);
             end);
         end
     end
 end
 
 --- @param module string
---- @param path string
---- @param cache boolean
---- @param callback ServerFileRequestCallback
+--- @param id string
+--- @param callback FileRequestCallback
 ---
 --- @return void
-function ModLoader.requestServerFile(module, path, cache, callback)
+function ModLoader.requestServerFile(module, id, callback)
     if IS_CLIENT then
         -- Set the callback for the file requested.
         local callbacks = CALLBACKS[module];
@@ -183,25 +217,27 @@ function ModLoader.requestServerFile(module, path, cache, callback)
             callbacks = {};
             CALLBACKS[module] = callbacks;
         end
-        callbacks[path] = callback;
+        callbacks[id] = callback;
 
         local packet = Packet(ModLoader.MODULE_ID, 'request_server_file', {
             module = module,
-            path = path,
-            cache = cache
+            id = id,
         });
 
-        packet:encrypt('ModLoader', function()
-            packet:sendToServer();
-        end);
+        packet:encrypt(ModLoader.SIMPLE_KEY, function() packet:sendToServer() end);
 
     elseif IS_SERVER then
-        local uri = 'ModLoader/mods/' .. module .. '/' .. path;
 
+        local uri = 'ModLoader/mods/' .. module .. '/' .. id;
+        local result;
         -- If the file is requested cached and is in the cache, grab it & send it.
-        if cache and cacheExists(module, path) then
-            local data = getCachedFile(module, path);
-            callback(module, path, ModLoader.RESULT_SUCCESS, data);
+        if cacheExists(module, id) then
+            local data = getCachedFile(module, id);
+            result = callback(ModLoader.RESULT_SUCCESS, data);
+            if result then
+                info('Caching file: ' .. id);
+                cacheFile(module, id, result);
+            end
             return;
         end
 
@@ -210,43 +246,51 @@ function ModLoader.requestServerFile(module, path, cache, callback)
         -- A nil reader indicates a bad path or a missing file.
         if not file then
             pcall(function()
-                callback(module, path, ModLoader.RESULT_FILE_NOT_FOUND, nil);
+                result = callback(ModLoader.RESULT_FILE_NOT_FOUND, nil);
+                if result then
+                    info('Caching file: ' .. id);
+                    cacheFile(module, id, result);
+                end
             end);
             return;
         end
 
-        local result = callback(module, path, ModLoader.RESULT_SUCCESS, file);
-        if result ~= nil then
-            file = result;
-        end
-
-        if cache then
-            print('[ModLoader] :: Caching file: ' .. path);
-            cacheFile(module, path, file);
+        result = callback(ModLoader.RESULT_SUCCESS, file);
+        if result then
+            info('Caching file: ' .. id);
+            cacheFile(module, id, result);
         end
     end
 end
 
 Events.OnClientCommand.Add(function(module, command, player, data)
+
+    -- ZedUtils.printLuaCommand(module, command, nil, data);
+
     if module ~= ModLoader.MODULE_ID then return end
+
     local packet = Packet(module, command, data);
-    packet:decrypt('ModLoader', function()
+    packet:decrypt(ModLoader.SIMPLE_KEY, function()
         if packet.command == 'request_server_file' then
-            local path = packet.data.path;
-            local cache = packet.data.cache;
-            onRequestServerFile(player, packet.data.module, path, cache);
+            local id = packet.data.id;
+            onRequestServerFile(player, packet.data.module, id);
         end
     end);
 end);
 
 Events.OnServerCommand.Add(function(module, command, data)
+
+    -- ZedUtils.printLuaCommand(module, command, nil, data);
+
     if module ~= ModLoader.MODULE_ID then return end
+
     local packet = Packet(module, command, data);
-    packet:decrypt('ModLoader', function()
+    packet:decrypt(ModLoader.SIMPLE_KEY, function()
+        -- print(packet:toJSON());
         if packet.command == 'request_server_file' then
-            local path = packet.data.path;
+            local id = packet.data.id;
             local result = packet.data.result;
-            onReceiveServerFile(packet.data.module, path, result, packet.data.data);
+            onReceiveServerFile(packet.data.module, id, result, packet.data.data);
         end
     end);
 end);
